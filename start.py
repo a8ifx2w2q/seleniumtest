c750bcf5888d03a76d89eacaa373564bbc5e56ec
a8ifx2w2q <85213210+a8ifx2w2q@users.noreply.github.com>
2021-06-02 17:54:20 +0530

    Update start.py

--- start.py
+++ start.py
@@ -2,14 +2,15 @@ import time
 import os
 import undetected_chromedriver as uc
 from selenium import webdriver
-
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 weburl = os.getenv('WEB_URL')
 
 check=1
 while(check>0):
     options = webdriver.ChromeOptions() 
     options.add_argument("start-maximized")
-    driver = uc.Chrome(options=options, executable_path=/app/.apt/usr/bin/google-chrome)
+    driver = uc.Chrome(options=options, executable_path=ChromeDriverManager().install())
     driver.get(weburl)
     time.sleep(1200)
     print("Times Run = ", check)
