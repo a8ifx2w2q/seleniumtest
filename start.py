a23981786d2de5fc59063f93cf47842ae4820558
a8ifx2w2q <85213210+a8ifx2w2q@users.noreply.github.com>
2021-06-02 19:23:19 +0530

    Update start.py

--- start.py
+++ start.py
@@ -12,7 +12,7 @@ options.add_argument("no-sandbox")
 options.add_argument("'useAutomationExtension', False")
 options.add_argument('"excludeSwitches", ["enable-logging", "enable-automation"]')
 options.add_argument('user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17"')
-driver = uc.Chrome(options=options, executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(options=options, executable_path=ChromeDriverManager().install())
 check=1
 while(check>0):
     driver.get(weburl)
