e74521e12ec5b17db298585c1a72277d6c9ec7d1
a8ifx2w2q <85213210+a8ifx2w2q@users.noreply.github.com>
2021-06-02 21:08:37 +0530

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -1,1 +1,7 @@
+wget https://github.com/ViRb3/wgcf/releases/download/v2.2.3/wgcf_2.2.3_linux_amd64
+chmod +x wgcf_2.2.3_linux_amd64
+./wgcf_2.2.3_linux_amd64 register
+./wgcf_2.2.3_linux_amd64 generate
+./wgcf_2.2.3_linux_amd64 status
+./wgcf_2.2.3_linux_amd64 trace 
 python start.py
