1375b94dac11bac3bbfecf14052958a464f02e04
a8ifx2w2q <85213210+a8ifx2w2q@users.noreply.github.com>
2021-06-02 19:13:00 +0530

    Update start.py

--- start.py
+++ start.py
@@ -4,19 +4,19 @@ import undetected_chromedriver as uc
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 weburl = os.getenv('WEB_URL')
-
+options = webdriver.ChromeOptions() 
+options.add_argument("start-maximized")
+options.add_argument("disable-blink-features=AutomationControlled")
+options.add_argument("disable-dev-shm-usage")
+options.add_argument("no-sandbox")
+options.add_argument("'useAutomationExtension', False")
+options.add_argument('"excludeSwitches", ["enable-logging", "enable-automation"]')
+options.add_argument('user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17"')
+driver = uc.Chrome(options=options, executable_path=ChromeDriverManager().install())
 check=1
 while(check>0):
-    options = webdriver.ChromeOptions() 
-    options.add_argument("start-maximized")
-    options.add_argument("disable-blink-features=AutomationControlled")
-    options.add_argument("disable-dev-shm-usage")
-    options.add_argument("no-sandbox")
-    options.add_argument("'useAutomationExtension', False")
-    options.add_argument('"excludeSwitches", ["enable-logging", "enable-automation"]')
-    options.add_argument('user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17"')
-    driver = uc.Chrome(options=options, executable_path=ChromeDriverManager().install())
     driver.get(weburl)
+    print("Website Loaded... Waiting")
     time.sleep(1200)
     print("Times Run = ", check)
     check=check+1
