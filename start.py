e43eca8cdc45ca04c7b26b56efb7f88a9c41d327
a8ifx2w2q <85213210+a8ifx2w2q@users.noreply.github.com>
2021-06-02 21:39:23 +0530

    Update start.py

--- start.py
+++ start.py
@@ -12,6 +12,10 @@ gChromeOptions = webdriver.ChromeOptions()
 gChromeOptions.add_argument("window-size=1920x1480")
 gChromeOptions.add_argument("disable-dev-shm-usage")
 gChromeOptions.add_argument("disable-blink-features=AutomationControlled")
+gChromeOptions.add_argument("no-sandbox")
+gChromeOptions.add_argument("'useAutomationExtension', False")
+gChromeOptions.add_argument('"excludeSwitches", ["enable-logging", "enable-automation"]')
+gChromeOptions.add_argument('user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17"')
 gDriver = webdriver.Chrome(
     chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
 )
@@ -19,6 +23,6 @@ check=1
 while(check>0):   
     gDriver.get(weburl)
     time.sleep(20)
-    gDriver.quit()
+    gDriver.close()
     print("Times Run = ", check)
     check=check+1
